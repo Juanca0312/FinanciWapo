02905658c21ff7e025e4e535337cef52aa2a83f1
Juanca0312 <jchernandez0312@gmail.com>
2020-10-30 16:33:57 -0500

    Listo CustomerInfo

--- src/main/java/com/econowapo/financiapp/service/UserService.java
+++ src/main/java/com/econowapo/financiapp/service/UserService.java
@@ -1,9 +1,12 @@
 package com.econowapo.financiapp.service;
 
+import com.econowapo.financiapp.model.CustomerInfo;
 import com.econowapo.financiapp.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface UserService {
     Page<User> getAllUsers(Pageable pageable);
@@ -11,5 +14,6 @@ public interface UserService {
     User createUser(User user);
     User updateUser(Long userId, User userRequest);
     ResponseEntity<?> deleteUser(Long userId);
+    List<CustomerInfo> getCustomersInfo();
 
 }
