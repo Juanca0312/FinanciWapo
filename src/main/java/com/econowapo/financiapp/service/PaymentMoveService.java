b81c85cb2071420929d713a5e42eaf8a2a305d61
Juanca0312 <jchernandez0312@gmail.com>
2020-11-10 17:59:22 -0500

    Edit PaymentMoves

--- src/main/java/com/econowapo/financiapp/service/PaymentMoveService.java
+++ src/main/java/com/econowapo/financiapp/service/PaymentMoveService.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface PaymentMoveService {
-    PaymentMove createPaymentMove(Long paymentId);
+    PaymentMove createPaymentMove(Long creditAccountId);
     PaymentMove updatePaymentMove(Long paymentId, Long paymentMoveId, PaymentMove paymentMove);
     List<PaymentInfo> getAllPaymentMoves();
     List<PaymentInfo> getAllPaymentMovesByCustomerId(Long customerId);
