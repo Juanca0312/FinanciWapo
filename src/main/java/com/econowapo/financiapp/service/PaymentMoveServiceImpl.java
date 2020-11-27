1513e8f6833f05c07b2acef0a0b01d4ebb422968
Juanca0312 <jchernandez0312@gmail.com>
2020-11-26 21:07:48 -0500

    Payment Move mejorado

--- src/main/java/com/econowapo/financiapp/service/PaymentMoveServiceImpl.java
+++ src/main/java/com/econowapo/financiapp/service/PaymentMoveServiceImpl.java
@@ -9,6 +9,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -17,10 +21,13 @@ import java.util.Optional;
 
 
 @Service
-public class PaymentMoveServiceImpl implements PaymentMoveService{
+public class PaymentMoveServiceImpl implements PaymentMoveService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private OrderDetailRepository orderDetailRepository;
 
     @Autowired
     private PaymentMoveRepository paymentMoveRepository;
@@ -41,13 +48,96 @@ public class PaymentMoveServiceImpl implements PaymentMoveService{
         Payment payment = paymentRepository.findByCreditAccountId(creditAccount.getId());
         List<CreditAccountMovement> creditAccountMovements = creditAccountMovementRepository.findByCreditAccountId(creditAccount.getId());
         List<CreditAccountMovement> creditAccountMovementsActives = new ArrayList<>();
-        for (CreditAccountMovement movement: creditAccountMovements){
-            if(movement.getState() == 1){
+        for (CreditAccountMovement movement : creditAccountMovements) {
+            if (movement.getState() == 1) {
                 creditAccountMovementsActives.add(movement);
             }
         }
+        //varianle que guarda el pago total del mes
+        double totalPago = 0;
+        //hallamos el interes para cada orden generada en el mes
+        for (CreditAccountMovement creditAccountMovement : creditAccountMovementsActives) {
+            Order order = creditAccountMovement.getOrder();
+            //hallamos el precio total de la orden
+            double totalOrder = 0;
+            List<Order_Detail> orderDetails = orderDetailRepository.findByOrderId(order.getId());
+            for (Order_Detail od : orderDetails) {
+                totalOrder += od.getArticle().getPrice() * od.getQuantity();
+            }
+            //traemos la diferencia de dias
+            Date lastDate = new Date();
+            Date firstDate = order.getGenerated_date();
 
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+            String fd = formatter.format(firstDate);
+            String ld = formatter.format(lastDate);
+
+            LocalDate d1 = LocalDate.parse(fd, DateTimeFormatter.ISO_LOCAL_DATE);
+            LocalDate d2 = LocalDate.parse(ld, DateTimeFormatter.ISO_LOCAL_DATE);
+            Duration diff = Duration.between(d1.atStartOfDay(), d2.atStartOfDay());
+            double dif = diff.toDays();
+            double capital = totalOrder;
+            double interestOrder = 0;
+
+            //validamos que tipo de cuenta es y realizamos el calculo
+            if (creditAccount.getInterest_rate() == 1) {//simple anual a;o ordinario 360
+                System.out.println("SIMPLE MANITO GAA");
+                double interestRate = creditAccount.getInterest_rate_value() / 100;
+                System.out.println(interestRate+ "DIFERENCIA DE DIAS");
+                double t = (dif) / 360;
+                System.out.println(dif + "DIFERENCIA DE DIAS");
+                interestOrder = capital * (1 + interestRate * t);
+                System.out.println(interestOrder + "VALOR FUTURO");
+
+            } else if (creditAccount.getInterest_rate() == 2) {//nominal, simepre capitalizacion diaria
+                System.out.println("NOMINAL MANITO GAA");
+                double interestRate = creditAccount.getInterest_rate_value() / 100;
+                double n = dif;
+                double m = 30;
+                double prev = 1 + interestRate / m;
+                interestOrder = capital * Math.pow(prev, n);
+                System.out.println(interestOrder);
+
+
+            } else {//efectiva
+                System.out.println("EFECTIVA MANITO GAA");
+                double interestRate = creditAccount.getInterest_rate_value() / 100;
+                System.out.println(interestRate + " interes rate");
+                double nt = dif;
+                System.out.println(nt + " diferencia de dias");
+                double nPeriodo = 30;
+                double prev = 1 + interestRate;
+                double sigu = nt / nPeriodo;
+                interestOrder = capital * Math.pow(prev, sigu);
+                System.out.println(interestOrder + " VALOR FUTURO ORDEN");
+
+            }
+            totalPago += interestOrder;
+            System.out.println(totalPago + "TOTAL PAGO");
+        }
+        System.out.println(totalPago);
+        paymentMove.setAmount(totalPago);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String fechaComoCadena = sdf.format(new Date());
+        paymentMove.setGenerated_date(fechaComoCadena);
+
+        //Cambiamos el estado de los movimientos a PAGADO (2)
+        for (CreditAccountMovement movement: creditAccountMovementsActives) {
+            movement.setState(2);
+            creditAccountMovementRepository.save(movement);
+        }
+
+        //Saldo Actual cambia a Saldo
+        creditAccount.setActual_balance(creditAccount.getBalance());
+        creditAccountRepository.save(creditAccount);
+
+        paymentMove.setPayment(payment);
+        paymentMoveRepository.save(paymentMove);
+
+        return paymentMove;
+
+        /*SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
         String fecha = formatter.format(date);
         paymentMove.setGenerated_date(fecha);
@@ -70,7 +160,7 @@ public class PaymentMoveServiceImpl implements PaymentMoveService{
             System.out.println(t);
             amount = capital*(1+interestRate*t);
             if(day > 2){
-                amount += 10;
+                //amount += 10;
             }
             System.out.println("AMONT");
             System.out.println(amount);
@@ -83,7 +173,7 @@ public class PaymentMoveServiceImpl implements PaymentMoveService{
             amount = capital*Math.pow(prev, n);
 
             if(day > 2){
-                amount += 10;
+                //amount += 10;
             }
 
         }
@@ -96,7 +186,7 @@ public class PaymentMoveServiceImpl implements PaymentMoveService{
             amount = capital * Math.pow(prev, sigu);
 
             if(day > 2){
-                amount += 10;
+                //amount += 10;
             }
         }
         paymentMove.setAmount(amount);
@@ -113,9 +203,9 @@ public class PaymentMoveServiceImpl implements PaymentMoveService{
 
         paymentMove.setPayment(payment);
         paymentMoveRepository.save(paymentMove);
+*/
 
 
-        return paymentMove;
 
     }
 
@@ -128,14 +218,14 @@ public class PaymentMoveServiceImpl implements PaymentMoveService{
     public List<PaymentInfo> getAllPaymentMoves() {
         List<PaymentInfo> paymentsInfo = new ArrayList<>();
         List<PaymentMove> payments = paymentMoveRepository.findAll();
-        for (PaymentMove pmovement: payments){
+        for (PaymentMove pmovement : payments) {
             PaymentInfo info = new PaymentInfo();
             info.setAmount(pmovement.getAmount());
             info.setGenerated_date(pmovement.getGenerated_date());
             info.setPaymentId(pmovement.getId());
             info.setState(pmovement.getState());
             Payment payment = pmovement.getPayment();
-            CreditAccount creditAccount =  payment.getCreditAccount();
+            CreditAccount creditAccount = payment.getCreditAccount();
             Customer customer = creditAccount.getCustomer();
             info.setCustomerName(customer.getName());
             paymentsInfo.add(info);
@@ -152,7 +242,7 @@ public class PaymentMoveServiceImpl implements PaymentMoveService{
         List<PaymentMove> paymentMoves = paymentMoveRepository.findByPaymentId(payment.getId());
         List<PaymentInfo> paymentsInfo = new ArrayList<>();
 
-        for (PaymentMove pmovement: paymentMoves) {
+        for (PaymentMove pmovement : paymentMoves) {
             PaymentInfo info = new PaymentInfo();
             info.setCustomerName(customer.getName());
             info.setAmount(pmovement.getAmount());
